7a4eaec822902ff69e520bc0b4c62deab7668d5c
Nikita <nikkika2011@mail.ru>
2019-12-07 12:15:47 +0300

    Init project.

---
+++ src/test/java/by/bsu/pizza/PizzaApplicationTests.java
@@ -0,0 +1,14 @@
+package by.bsu.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("THIS IS TEST");
+    }
+
+}
