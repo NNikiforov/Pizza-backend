e106aefa2b936a36786ce5b07e5a7bfd459ac12f
Mikita Nikifarau <Mikita.Nikifarau@aerlingus.com>
2019-12-13 19:05:52 +0300

    Fix travis yml.

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,4 +1,3 @@
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-docker login -u nnikiforov --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web -—app $HEROKU_APP_NAME
+docker login -username=nnikiforov -password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web -app $HEROKU_APP_NAME
+heroku container:release web -app $HEROKU_APP_NAME
