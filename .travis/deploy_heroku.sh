5c236147f5ddf1563e49b55299de24a4e0a5fee0
Mikita Nikifarau <Mikita.Nikifarau@aerlingus.com>
2019-12-13 17:54:50 +0300

    Automation HEROKU delpoyment.

---
+++ .travis/deploy_heroku.sh
@@ -0,0 +1,3 @@
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+docker login -u nnikiforov --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APP_NAME
