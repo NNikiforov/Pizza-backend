9f964283623ab6c7742724daf53c4fb99c8251a6
Nikita <nikkika2011@mail.ru>
2019-12-25 02:15:22 +0300

    Update config classes.

---
+++ src/main/java/by/bsu/pizza/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package by.bsu.pizza.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final DataSource dataSource;
+
+	@Autowired(required = false)
+	public SecurityConfig(@Nullable DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
+	}
+
+}
